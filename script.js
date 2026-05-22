342062f9ef3de10591b6d88d6292db19300e80a9
HailuBen <b.hailu.pk@gmail.com>
2026-05-22 16:38:12 -0400

    add variables, arithmetic

---
+++ script.js
@@ -0,0 +1,31 @@
+let greeting = "Hello 113!";
+console.log(greeting);
+
+
+let studentName="Charles";
+let studentAge=20;
+let isEnrolled=true;
+console.log("Name: "+studentName+"\nAge: "+studentAge+"\nEnrolled: "+ (isEnrolled ? "Yes" : "No"));
+
+
+let interests = ["gaming","music","guitar","webdev","cooking"]
+
+let person = {
+    firstName: "Charles",
+    lastName: "Anderson"
+};
+
+
+// Arithmetic
+console.log("\nAddition +")
+console.log("Student Age: "+studentAge+"\n+ 20" + "\n= " +(studentAge+20));
+
+console.log("\nSubtraction -")
+console.log("Student Age: "+studentAge+"\n- 10" + "\n= "+(studentAge-10));
+
+console.log("\nMultiplication *")
+console.log("Student Age: "+studentAge+"\n* 10" + "\n= "+(studentAge*10));
+
+console.log("\nDivision /")
+console.log("Student Age: "+studentAge+"\n/ 2" + "\n= "+(studentAge/2));
+
